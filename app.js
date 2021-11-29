45cbf7cf6d70db057fbe563777b80e816da51129
swati <91478967+swati-kaithwas@users.noreply.github.com>
2021-11-29 14:50:34 +0530

    comitt2

--- app.js
+++ app.js
@@ -3,6 +3,96 @@ const  mongoose = require("mongoose");
 const connect = () => {
     return mongoose.connect("mongodb://localhost:27017/");
 };
+//userschema
+const userSchema = new mongoose.Schema(
+    {
+        first_name: { type: String, required: true},
+        last_name: { type: String, required: false},
+        age:{ type:Number,required: true},
+        email: {type:String,require: true, unique: true},
+        gender: { type:String,required: false,default:"Female"},
+
+        
+    },
+    {
+        versionKey: false,
+        timestamp: true,
+    }
+);
+const User =mongoose.model("user",userSchema);//user
+//companyshema 
+
+const companySchema = new mongoose.Schema(
+    {
+        user_id: {
+         type: mongoose.Schema.Types.ObjectId,
+         ref: "user",
+         required: true,
+        },
+      Company_name: {type:String, required: true},
+      company_type: {type: String,required:true},
+
+      
+        
+    },
+    {
+        versionKey: false,
+        timestamp: true,
+    }
+);
+const Company =mongoose.model("company",companySchema);//company
+
+
+const workerSchema = new mongoose.Schema(
+    {
+        Company_name: {type:String, required: true},
+        company_type: {type: String,required:true},
+  
+        user_id: {
+         type: mongoose.Schema.Types.ObjectId,
+         ref: "user",
+         required: true,
+        },
+     company_id: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "company",
+        required: true,
+     },
+      
+        
+    },
+    {
+        versionKey: false,
+        timestamp: true,
+    }
+);
+const Worker =mongoose.model("worker",workerSchema);//company
+
+
+const skillSchema = new mongoose.Schema(
+    {
+        Company_name: {type:String, required: true},
+        company_type: {type: String,required:true},
+        job_type: {type:String,required:true},
+        user_id: {
+         type: mongoose.Schema.Types.ObjectId,
+         ref: "user",
+         required: true,
+        },
+     company_id: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "company",
+        required: true,
+     },
+      
+        
+    },
+    {
+        versionKey: false,
+        timestamp: true,
+    }
+);
+const Skill =mongoose.model("skill",skillSchema);//company
 
 const app = express();
 app.use(express.json());//midlle
